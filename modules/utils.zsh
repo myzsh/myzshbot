8d59a4f98850db1bff5a2f625c72da4f7d211668
Matt <brimstone@the.narro.ws>
2014-12-18 18:27:09 -0500

    Adding support for colors in irc messages.

--- modules/utils.zsh
+++ modules/utils.zsh
@@ -1,4 +1,24 @@
 msg(){
-	echo "Sending '$2' to $1"
-	print "PRIVMSG $1 :$2" >&$ircfd
+	local target="$1"
+	local msg="$2"
+
+	msg="${msg//{white\}/\x030}"
+	msg="${msg//{black\}/\x031}"
+	msg="${msg//{blue\}/\x032}"
+	msg="${msg//{green\}/\x033}"
+	msg="${msg//{red\}/\x034}"
+	msg="${msg//{brown\}/\x035}"
+	msg="${msg//{purple\}/\x036}"
+	msg="${msg//{orange\}/\x037}"
+	msg="${msg//{yellow\}/\x038}"
+	msg="${msg//{lgreen\}/\x039}"
+	msg="${msg//{lcyan\}/\x0310}"
+	msg="${msg//{lblue\}/\x0311}"
+	msg="${msg//{lpurple\}/\x0312}"
+	msg="${msg//{grey\}/\x0313}"
+	msg="${msg//{lgrey\}/\x0314}"
+	msg="${msg//{reset\}/\x03}"
+	
+	echo "Sending '$msg' to $target"
+	print "PRIVMSG $target :$msg" >&$ircfd
 }
